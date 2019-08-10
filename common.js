885d28d0fbce127360d5a4177c5cbd6ab63f1103
Ragav Kumar <27591832+ragav-kumar@users.noreply.github.com>
2019-08-10 11:37:24 -0700

    WORK

--- common.js
+++ common.js
@@ -46,10 +46,54 @@ function PartFeature(part, feature) {
 		feature: feature,
 	}
 }
+
+var pool = mysql.createPool({
+	connectionLimit: 10,
+	host:     process.env.MYSQL_host,
+	user:     process.env.MYSQL_user,
+	password: process.env.MYSQL_password,
+	database: process.env.MYSQL_database,
+});
+
+const connection = {
+    query: function () {
+        var queryArgs = Array.prototype.slice.call(arguments),
+            events = [],
+            eventNameIndex = {};
+
+        pool.getConnection(function (err, conn) {
+            if (err) {
+                if (eventNameIndex.error) {
+                    eventNameIndex.error();
+                }
+            }
+            if (conn) { 
+                var q = conn.query.apply(conn, queryArgs);
+                q.on('end', function () {
+                    conn.release();
+                });
+
+                events.forEach(function (args) {
+                    q.on.apply(q, args);
+                });
+            }
+        });
+
+        return {
+            on: function (eventName, callback) {
+                events.push(Array.prototype.slice.call(arguments));
+                eventNameIndex[eventName] = callback;
+                return this;
+            }
+        };
+    }
+};
+
 module.exports = {
 	Boat,
 	Part,
 	BoatPart,
 	PartFeature,
 	Spec_Heading,
+	connection,
 }
--- routes/sql.js
+++ routes/sql.js
@@ -3,26 +3,55 @@ var router = express.Router();
 
 const {performance} = require('perf_hooks');
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
 	host:     process.env.MYSQL_host,
 	user:     process.env.MYSQL_user,
 	password: process.env.MYSQL_password,
 	database: process.env.MYSQL_database,
-});
+});*/
 const dbSQL = require('../dbSQL');
+const common = require('../common');
+
+/*const pool = mysql.createPool({
+	connectionLimit: 10,
+	host:     process.env.MYSQL_host,
+	user:     process.env.MYSQL_user,
+	password: process.env.MYSQL_password,
+	database: process.env.MYSQL_database,
+})*/
 
 const queryHandler = (req, res, next) => {
-	connection.connect();
+	//connection.connect();
 	t0 = performance.now();
-	connection.query(req.sql, (err, rows, fields) => {
-		if (err) throw err;
 
-		res.json({
-			requestTime: performance.now() - t0,
-			...rows,
-		});
-	})
-	connection.end();
+	common.query(req.sql)
+		.on('result', rows => {
+			res.json({
+				success: true,
+				requestTime: performance.now() - t0,
+				data: rows,
+			});
+		})
+		.on('error', err => {
+			res.json({
+				success: false,
+				error: err,
+			})
+		})
+	;
+	/*
+	pool.getConnection((err, connection) => {
+		connection.query(req.sql, (err, rows, fields) => {
+			if (err) throw err;
+	
+			res.json({
+				requestTime: performance.now() - t0,
+				...rows,
+			});
+			connection.release();
+		})
+	});*/
+	//connection.end();
 }
 router.get('/', queryHandler);
 router.post('/', queryHandler);
