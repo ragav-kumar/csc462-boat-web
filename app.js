4c61b7c12ebbcaca73a90bc56f4e942fb63633b0
Ragav Kumar <27591832+ragav-kumar@users.noreply.github.com>
2019-08-10 10:27:11 -0700

    route

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var queryRouter = require('./routes/query');
 var csvRouter = require('./routes/csv');
 var apiRouter = require('./routes/api');
+var sqlRouter = require('./routes/sql');
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use('/', indexRouter);
 app.use('/query', queryRouter);
 app.use('/csv', csvRouter);
 app.use('/api', apiRouter);
+app.use('/sql', sqlRouter);
 
 module.exports = app;
